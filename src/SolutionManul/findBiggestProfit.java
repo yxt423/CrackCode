b0f16860de18be43d4520afebeeca9b8b89a7a0d
Xintong Yu <yxt423@gmail.com>
2014-01-21 21:38:16 -0500

    findBiggestProfit

---
+++ src/SolutionManul/findBiggestProfit.java
@@ -0,0 +1,13 @@
+package SolutionManul;
+
+/*Given an array of integers, where each element represents the
+ *  stock price on consecutive days. Write a function to find 
+ *  the maximum profit if we're allowed to buy exactly once and
+ *   sell exactly once over the time period.
+ *   */
+public class findBiggestProfit {
+	
+	public static int findProfit(int[] days){
+		
+	}
+}
