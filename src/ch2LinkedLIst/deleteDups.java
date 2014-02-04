7e45b679e1003f9e1f358c426e048323ca08a277
Xintong Yu <yxt423@gmail.com>
2014-02-03 22:57:57 -0500

    ch2 deleteDupNodes more methods

--- src/ch2LinkedLIst/deleteDups.java
+++ src/ch2LinkedLIst/deleteDups.java
@@ -1,10 +1,12 @@
 package ch2LinkedLIst;
 
 import java.util.Hashtable;
+import java.util.HashSet;
 
 public class deleteDups  {
 	
-	public static void deleteDupNodes(Node n){
+	// use hash table //is hash table necessary?
+	public static void deleteDupNodesA(Node n){
 		Node previous = null;
 		Hashtable<Integer, Boolean> table = new Hashtable<Integer, Boolean>();
 		while(n != null){
@@ -15,6 +17,39 @@ public class deleteDups  {
 				previous = n;
 			}
 			n = n.next;
+		}
+	}
+	
+	//use hashset
+	public static void deleteDupNodesB(Node n){
+		HashSet<Integer> set = new HashSet<Integer>();
+		Node previous = null;
+		while(n != null){
+			if(set.contains(n.value)){
+				previous.next = n.next;
+			}
+			else{
+				set.add(n.value);
+				previous = n;
+			}
+			n = n.next;
+		}
+	}
+	
+	public static void deleteDupNodesC(Node n){
+		Node p1 = n;
+		while(p1 != null){
+			Node p2 = p1.next;
+			Node previous = p1;
+			while(p2 != null){
+				while(p2.value == p1.value){ // the third while ensures that, if 1,1,1 exists, remove all the other 2 1s.
+					previous.next = p2.next;
+					p2 = p2.next;
+				}
+				p2 = p2.next;
+				previous = previous.next;
+			}
+			p1 = p1.next;
 		}
 	}
 
@@ -29,9 +64,11 @@ public class deleteDups  {
 		list.insertFirst(4);
 		list.insertFirst(4);
 		list.insertFirst(5);
+		list.insertFirst(4);
+		list.insertFirst(5);
 		
 		list.displayList();
-		deleteDupNodes(list.getHead());
+		deleteDupNodesC(list.getHead());
 		list.displayList();
 	}
 
