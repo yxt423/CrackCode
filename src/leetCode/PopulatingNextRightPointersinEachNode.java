5ad8c929302fc8a1e2594dcc7080ad41f594215c
Xintong Yu <yxt423@gmail.com>
2014-02-17 20:41:05 -0500

    PopulatingNextRightPointersinEachNode

---
+++ src/leetCode/PopulatingNextRightPointersinEachNode.java
@@ -0,0 +1,20 @@
+package leetCode;
+
+public class PopulatingNextRightPointersinEachNode {
+    public void connect(TreeLinkNode root) {
+        if (root == null || root.left == null) {
+            return;
+        }
+        
+        root.left.next = root.right;
+        
+        if (root.next == null) {
+            root.right.next = null;
+        } else {
+            root.right.next = root.next.left;
+        }
+        
+        connect(root.left);
+        connect(root.right);
+    }
+}
