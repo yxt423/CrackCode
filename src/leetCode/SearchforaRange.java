6f9fd0113db11f4e9157dc16f3f57cf544798922
Xintong Yu <yxt423@gmail.com>
2014-02-17 19:19:57 -0500

    SearchforaRange

---
+++ src/leetCode/SearchforaRange.java
@@ -0,0 +1,54 @@
+package leetCode;
+
+public class SearchforaRange {
+    public int[] searchRange(int[] A, int target) {
+        int start, end, mid;
+        int[] bound = {-1, -1};
+        
+        if (A == null || A.length == 0) {
+            return bound;
+        }
+        
+        //search for left bound
+        start = 0;
+        end = A.length - 1;
+        while (start + 1 < end) {
+            mid = start + (end - start) / 2;
+            if (A[mid] == target) {
+                end = mid;
+            } else if (A[mid] < target) {
+                start = mid;
+            } else {
+                end = mid;
+            }
+        }
+        if (A[start] == target) {
+            bound[0] = start;
+        } else if (A[end] == target) {
+            bound[0] = end;
+        } else {
+            return bound;
+        }
+        
+        //search for right bound
+        start = 0;
+        end = A.length - 1;
+        while (start + 1 < end) {
+            mid = start + (end - start) / 2;
+            if (A[mid] == target) {
+                start = mid;
+            } else if (A[mid] < target) {
+                start = mid;
+            } else {
+                end = mid;
+            }
+        }
+        if (A[end] == target) {
+            bound[1] = end;
+        } else if (A[start] == target) {
+            bound[1] = start;
+        }
+        
+        return bound;
+    }
+}
