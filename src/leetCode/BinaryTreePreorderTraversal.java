22cdcfeabc91ae153977ec501a868979a01de937
Xintong Yu <yxt423@gmail.com>
2014-02-18 11:31:04 -0500

    BinaryTreePreorderTraversal 1,2,3

--- src/leetCode/BinaryTreePreorderTraversal.java
+++ src/leetCode/BinaryTreePreorderTraversal.java
@@ -1,10 +1,12 @@
 package leetCode;
 
 import java.util.ArrayList;
+import java.util.Stack;
 
 public class BinaryTreePreorderTraversal {
-	
-	public ArrayList<Integer> preorderTraversal(TreeNode root) {
+
+    // solution 1
+    public ArrayList<Integer> preorderTraversal1(TreeNode root) {
         ArrayList<Integer> result = new ArrayList<Integer>();
         traverse(root, result);
         return result;
@@ -18,5 +20,43 @@ public class BinaryTreePreorderTraversal {
         result.add(root.val);
         traverse(root.left, result);
         traverse(root.right, result);
+    }
+    
+    // solution 2, Divide & Conquer
+    public ArrayList<Integer> preorderTraversal2(TreeNode root) { 
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        if (root == null) {
+            return result;
+        }
+        
+        ArrayList<Integer> left = preorderTraversal(root.left);
+        ArrayList<Integer> right = preorderTraversal(root.right);
+        
+        result.add(root.val);
+        result.addAll(left);
+        result.addAll(right);
+        return result;
+    }
+    
+    // solution 3, non-recursive, using stack
+    public ArrayList<Integer> preorderTraversal(TreeNode root) { 
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        if (root == null) {
+            return result;
+        }
+        
+        Stack<TreeNode> s = new Stack<TreeNode>();
+        s.push(root);
+        while (!s.isEmpty()) {
+            TreeNode cur = s.pop();
+            result.add(cur.val);
+            if (cur.right != null) {
+                s.push(cur.right);
+            }
+            if (cur.left != null) {
+                s.push(cur.left);
+            }
+        }
+        return result;
     }
 }
