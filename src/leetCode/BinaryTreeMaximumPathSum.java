5ee719e2f01f0e23221337a040e1e5597713b1fa
Xintong Yu <yxt423@gmail.com>
2014-02-18 14:13:50 -0500

    BinaryTreeMaximumPathSum

---
+++ src/leetCode/BinaryTreeMaximumPathSum.java
@@ -0,0 +1,33 @@
+package leetCode;
+
+public class BinaryTreeMaximumPathSum {
+    private class ResultType {
+        int singlePath, maxPath;
+        ResultType(int s, int m) {
+            this.singlePath = s;
+            this.maxPath = m;
+        }
+    }
+    
+    private ResultType helper(TreeNode root) {
+        if (root == null) {
+            return new ResultType(0, Integer.MIN_VALUE);
+        }
+        
+        ResultType left = helper(root.left);
+        ResultType right = helper(root.right);
+        
+        int singlePath = Math.max(left.singlePath, right.singlePath) + root.val;
+        singlePath = Math.max(singlePath, 0);
+        
+        int maxPath = Math.max(left.maxPath, right.maxPath);
+        maxPath = Math.max(maxPath, left.singlePath + right.singlePath + root.val);
+        
+        return new ResultType(singlePath, maxPath);
+    }
+    
+    public int maxPathSum(TreeNode root) {
+        ResultType result = helper(root);
+        return result.maxPath;
+    }
+}
