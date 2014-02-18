7048cb98a955ca2f348b3d79a3a43e63f96cd5bb
Xintong Yu <yxt423@gmail.com>
2014-02-17 20:12:49 -0500

    SearchA2DMatrix

--- src/leetCode/SearchA2DMatrix.java
+++ src/leetCode/SearchA2DMatrix.java
@@ -1,66 +1,38 @@
 package leetCode;
 
 public class SearchA2DMatrix {
-	
-	public static boolean searchMatrix(int[][] matrix, int target) {
-        if (matrix == null || matrix.length == 0){
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix == null || matrix.length < 1 || matrix[0].length < 1) {
             return false;
         }
         
+        int rowNum = matrix.length;
+        int colNum = matrix[0].length;
         int start = 0;
-        int end = matrix.length - 1;
+        int end = rowNum * colNum - 1;
         int mid;
-        int row = -1;
+        int ele;
         
-        while (start + 1 < end){
-            mid = start + (end - start)/2;
-            if (matrix[mid][0] == target){
-                row = mid;
-                break;
-            } else if (matrix[mid][0] < target){
-                start = mid;
-            } else {
-                end = mid;
-            }
-        }
-        
-        if (row == -1){
-            if (matrix[end][0] <= target){
-                row = end;
-            } else {
-                row = start;
-            }
-        }
-        
-        start = 0;
-        end = matrix[0].length - 1;
-        while (start + 1 < end){
-            mid = start + (end - start)/2;
-            if (matrix[row][mid] == target){
+        while (start + 1 < end) {
+            mid = start + (end - start) / 2;
+            ele = matrix[mid / colNum][mid % colNum];
+            if (ele == target) {
                 return true;
-            } else if (matrix[row][mid] < target){
+            } else if (ele < target) {
                 start = mid;
             } else {
                 end = mid;
             }
         }
         
-        if (matrix[row][start] == target || matrix[row][end] == target){
+        ele = matrix[start / colNum][start % colNum];
+        if (ele == target) {
             return true;
-        } else {
-            return false;
         }
+        ele = matrix[end / colNum][end % colNum];
+        if (ele == target) {
+            return true;
+        }
+        return false;
     }
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int[][] M = {{1,3,5,7},{10,11,16,20},{23,30,34,50}};
-		if (searchMatrix(M,30)){
-			System.out.println("true");
-		} else {
-			System.out.println("false");
-		}
-		
-	}
-
 }
